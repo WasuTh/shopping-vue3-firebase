a996ad6ee9c23dffb796ecec8bf7cd8eb37241a5
WasuTh <146047771+WasuTh@users.noreply.github.com>
2025-08-30 15:13:17 +0700

    feat: add order store and update view-admin-order

--- src/views/admin/order/DetailView.vue
+++ src/views/admin/order/DetailView.vue
@@ -1,8 +1,69 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute, RouterLink } from 'vue-router'
+
+import { useAdminOrderStore } from '@/stores/admin/order';
+
 import AdminLayout from '@/layouts/AdminLayout.vue'
+
+const route = useRoute()
+
+const adminOrder = useAdminOrderStore()
+
+const orderIndex = ref(-1)
+const orderData = ref({})
+
+onMounted(() => {
+    if (route.params.id) {
+        orderIndex.value = parseInt(route.params.id)
+
+        orderData.value = adminOrder.getOrder(orderIndex.value)
+    }
+})
 </script>
 <template>
     <AdminLayout>
-        Orders Detail
+        <div class="m-10 p-10 shadow-2xl">
+            <div class="mb-10 text-3xl font-bold">Orders id: {{ route.params.id }}</div>
+            <div class="p-5 grid grid-cols-2 gap-5 border-t">
+                <div>
+                    <div class="font-bold">Order Date</div>
+                    {{ orderData.updatedAt }}
+                </div>
+                <div>
+                    <div class="font-bold">Order Number</div>
+                    {{ orderData.no }}
+                </div>
+                <div>
+                    <div class="font-bold">Payment Method</div>
+                    {{ orderData.paymentMethod }}
+                </div>
+                <div>
+                    <div class="font-bold">Address</div>
+                    {{ orderData.address }}
+                </div>
+            </div>
+            <div v-for="order in orderData.products"
+                class="p-5 flex justify-between border-t">
+                <div class="flex">
+                    <img :src="order.imageUrl">
+                    <div class="ml-5">
+                        <div>{{ order.name }}</div>
+                        <div>{{ order.description }}</div>
+                    </div>
+                </div>
+                <div>จำนวน {{ order.quantity }} ชิ้น</div>
+                <div>{{ order.price }}</div>
+            </div>
+            <div class="p-5 flex justify-between font-bold border-t">
+                <span>ราคาสิ้นค้าทั้งหมด</span>
+                <div>ราคา {{ orderData.totalPrice }} B</div>
+            </div>
+            <div class="w-full p-5 flex justify-end">
+                <RouterLink :to="{ name: 'admin-orders-list' }"
+                    class="btn rounded-lg">Back
+                </RouterLink>
+            </div>
+        </div>
     </AdminLayout>
 </template>
--- src/views/admin/order/ListView.vue
+++ src/views/admin/order/ListView.vue
@@ -1,8 +1,35 @@
 <script setup>
+import { RouterLink } from 'vue-router';
+
+import { useAdminOrderStore } from '@/stores/admin/order';
+
 import AdminLayout from '@/layouts/AdminLayout.vue'
+import Table from '@/components/Table.vue';
+
+const adminOrder = useAdminOrderStore()
+
+console.log(adminOrder.list[0])
+
+const headersName = ['Name','Images','Price','Status','Updated at',]
 </script>
 <template>
     <AdminLayout>
-        Orders list
+        <div class="p-10">
+            <span class="text-3xl font-bold">Orders List</span>
+            <Table :headers="headersName">
+                <tr v-for="(order, index) in adminOrder.list"
+                    class="text-center">
+                    <td>{{ order.customerName }}</td>
+                    <td>{{ order.totalPrice }}</td>
+                    <td>{{ order.status }}</td>
+                    <td>{{ order.updatedAt }}</td>
+                    <td>
+                        <RouterLink :to="{ name: 'admin-order-detail', params: { id: index } }" 
+                            class="btn w-full rounded-md">See detail
+                        </RouterLink>
+                    </td>
+                </tr>
+            </Table>
+        </div>
     </AdminLayout>
 </template>
